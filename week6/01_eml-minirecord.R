95c4ad6ac392b01ae9eef20c4a44467e3cf4668e
Renata Curty <63265343+rcurty@users.noreply.github.com>
2023-05-06 13:44:04 -0700

    files for week 6

---
+++ week6/01_eml-minirecord.R
@@ -0,0 +1,22 @@
+# Required packages
+library(EML)
+library(emld)
+
+# Below is a minimal valid EML record with a tile, creator and contact info.
+
+me <- list(individualName = list(givenName = "Renata", surName = "Curty"))
+eml_mini <- list(dataset = list(
+  title = "This is a minimal valid EML dataset record",
+  creator = me,
+  contact = me)
+)
+
+#We then need to run 
+write_eml(eml_mini, "eml_mini.xml")
+eml_validate("eml-mini.xml")
+
+#We should get [1] TRUE with no erros and a min.xml record
+
+# As we learned this is too barebones, and not enough for findability and interoperability.
+# We need richer metadata!
+# Time to open the eml-losapio.R script file.
