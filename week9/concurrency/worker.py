910f55cbc25b6a96d4ae78d3bbe0bd5ced3f9e92
Greg Janée <gjanee@ucsb.edu>
2023-05-19 16:00:40 -0700

    Rename variable

--- week9/concurrency/worker.py
+++ week9/concurrency/worker.py
@@ -10,7 +10,7 @@ num_reps = int(sys.argv[1])
 delta = int(sys.argv[2])
 with_transactions = (len(sys.argv) >= 4 and sys.argv[3] == "wt")
 
-rg = random.SystemRandom()
+rng = random.SystemRandom()
 
 conn = sqlite3.connect("concurrency.db", isolation_level=None)
 c = conn.cursor()
@@ -19,9 +19,9 @@ for _ in range(num_reps):
     if with_transactions:
         c.execute("BEGIN IMMEDIATE")
     value = c.execute("SELECT value FROM my_table").fetchone()[0]
-    time.sleep(rg.random()*.025)
+    time.sleep(rng.random()*.025)
     value += delta
     c.execute("UPDATE my_table SET value = ?", (value,))
     if with_transactions:
         c.execute("COMMIT")
-    time.sleep(rg.random()*.025)
+    time.sleep(rng.random()*.025)
